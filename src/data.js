db951d3bcf05d726ad2a553d3b7d771e45ae721b
clint <clintjackson601@gmail.com>
2025-08-15 04:16:56 -0500

    added data

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export default [
+{
+    id: 1,
+    name: 'Movie Theater Website',
+    imgUrl: 'movie.png',
+    description: 'A front-end movie theater page.',
+    sourceCode: 'https://github.com/clint601/showtime',
+    livePage: 'https://showtime-a8laiynht-clints-projects-d82733ec.vercel.app/'
+},
+{
+    id: 2,
+    name: 'Movie Theater Website',
+    imgUrl: 'movie.png',
+    description: 'A front-end movie theater page using react.',
+    sourceCode: 'https://github.com/clint601/showtime',
+    livePage: 'https://showtime-a8laiynht-clints-projects-d82733ec.vercel.app/'
+},
+{
+    id: 3,
+    name: 'Restaurant',
+    imgUrl: 'Restaurant.png',
+    description: 'A front-end Restaurant page.',
+    sourceCode: 'https://github.com/clint601/foodboot',
+    livePage: 'https://clint601.github.io/foodboot/'
+},
+{
+    id: 4,
+    name: 'Detroix',
+    imgUrl: 'Detroix.png',
+    description: 'This is my detroix page where I use bootstrap and css.',
+    sourceCode: 'https://github.com/clint601/bootDextroix',
+    livePage: 'https://clint601.github.io/bootDextroix/'
+},
+{
+    id: 5,
+    name: 'Bloom',
+    imgUrl: 'Bloom.png',
+    description: 'This is my bloom page where I use bootstrap and css.',
+    sourceCode: 'https://clint601.github.io/bootbloom/',
+    livePage: 'https://clint601.github.io/foodboot/'
+},
+{
+    id: 6,
+    name: 'FakeZon',
+    imgUrl: 'FakeZon.png',
+    description: 'This is my FakeZon page where I use bootstrap and css.',
+    sourceCode: 'https://github.com/clint601/foodboot',
+    livePage: 'https://clint601.github.io/foodboot/'
+},
+
+]
